4faa11461ca0605a57a202a5874cb06c08882a4b
Gildo Fiorito <fyskij@gmail.com>
2011-03-29 00:26:12 +0200

    yeah²

---
+++ lib/helpers.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+def ghet u
+  uri = URI.parse(u)
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+
+  response = http.request(request)
+  body = JSON.parse(response.body)
+end
+
+def explode hash
+  hash.each do |k,v|
+    self.class.send(:define_method, k, proc{v})
+  end
+end
