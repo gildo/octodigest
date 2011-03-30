6301e84612dc51ce90dc2b0160d6bbfa9d6c4886
Gildo Fiorito <fyskij@gmail.com>
2011-03-30 20:13:30 +0200

    broken

--- lib/app.rb
+++ lib/app.rb
@@ -3,43 +3,47 @@ require 'padrino-core/application/rendering'
 require 'erb'
 require 'json'
 
-set :views,  'views'
-enable :static
-register Padrino::Rendering
+class Octodigest < Sinatra::Application
+  set :views,  'views'
+  enable :static
+  register Padrino::Rendering
 
-helpers do
-  require './lib/helpers'
-end
-
-get "/" do
-  erb :index
-end
-
-post "/" do
-  redirect ("#{h params[:user]}/#{h params[:repo]}")
-end
-
-get "/:user/:repo" do
-  @data = ghet("http://github.com/api/v2/json/repos/show/#{h params[:user]}/#{h params[:repo]}/contributors")
-  if @data.has_key? "error"
-    @title = "Not Found..."
-    erb :nf
-  else
-    @title = "#{h params[:user]}/#{h params[:repo]}"
-    erb :repo
+  helpers do
+    require './lib/helpers'
   end
-end
 
-get "/:user/:repo/:tag" do
-  tagger
-  if @tcommits.include? "error"
-    @title = "Not found..."
-    erb :nf
-  else
-    @title = "#{h params[:user]}/#{h params[:repo]} #{h params[:tag]}"
-    erb :tag
+  get "/" do
+    erb :index
   end
-end
-not_found do
-  erb :nf
+
+  post "/" do
+    redirect ("#{h params[:user]}/#{h params[:repo]}")
+  end
+
+  get "/:user/:repo" do
+    @data = ghet("http://github.com/api/v2/json/repos/show/#{h params[:user]}/#{h params[:repo]}/contributors")
+    if @data.has_key? "error"
+      @title = "Not Found..."
+      erb :nf
+    else
+      @title = "#{h params[:user]}/#{h params[:repo]}"
+      erb :repo
+    end
+  end
+
+  get "/:user/:repo/:tag" do
+    tagger
+    if @tcommits.include? "error"
+      @title = "Not found..."
+      erb :nf
+    else
+      @title = "#{h params[:user]}/#{h params[:repo]} #{h params[:tag]}"
+      erb :tag
+    end
+  end
+
+  not_found do
+    erb :nf
+  end
+
 end
